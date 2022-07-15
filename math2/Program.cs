69e88990246225da892532fdb9c6c1e31b9467b9
vishmi.11dec@gmail.com <vishmi.11dec@gmail.com>
2022-07-15 17:11:40 +0530

    FInal

---
+++ math2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static mathdll.Class1;
+namespace math2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            mathdll.Class1 c = new mathdll.Class1(2, 3);
+            Console.WriteLine("added:"+c.add(2,3));
+            Console.WriteLine("multiply:" + c.multiply(2, 3));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ mathdll/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mathdll
+{
+    public class Class1
+    {
+        public int x, y;
+        public Class1(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int multiply(int a, int b)
+        {
+            return a * b;//multi
+        }
+    }
+}
